4e82633d7900429c9e248df716bb473f20f4c4af
Kun Yu Chen <96057963+ouo314@users.noreply.github.com>
2024-08-07 21:39:48 +0800

    Add files via upload

--- ESP32_scope.ino
+++ ESP32_scope.ino
@@ -1,27 +1,39 @@
-const int analogPin = 36;    
-const int pwmPin = 39;       
+#include <driver/adc.h>
+#include "esp_adc_cal.h"
 
-const int pwmFrequency = 1000; 
-const int pwmResolution = 8;  
-const int pwmDelay = 10;      
+const int pwmPin = 39;
+const int pwmFrequency = 10;  
+const int pwmResolution = 8;
+const int samplesPerStep = 50;
+
+
+esp_adc_cal_characteristics_t adc_chars;
 
 void setup() {
   Serial.begin(115200);
-  analogReadResolution(12);   
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_0);
   ledcSetup(0, pwmFrequency, pwmResolution);
   ledcAttachPin(pwmPin, 0);
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
 }
 
 void loop() {
-  for(int i = 0; i < 256; i++) {
-    ledcWrite(0, i);          
-    int adcValue = analogRead(analogPin); 
+  for (int i = 0; i < 256; i++) {
+    ledcWrite(0, i);  
+    long sum = 0;
+    for (int j = 0; j < samplesPerStep; j++) {
+      int val = adc1_get_raw(ADC1_CHANNEL_0);  
+      sum += val;
+      delayMicroseconds(100);
+    }
+    int avgAdcValue = sum / samplesPerStep; 
+    int scaledAdcValue = map(avgAdcValue, 0, 4095, 0, 255);
     Serial.print("PWM: ");
     Serial.print(i);
     Serial.print(" ADC: ");
-    Serial.println(adcValue);  
-    delay(pwmDelay);          
+    Serial.println(scaledAdcValue);
   }
-  
-  delay(100);                
+
+  delay(100);
 }
