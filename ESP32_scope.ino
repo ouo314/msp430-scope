478c679089012f63cdd0386c5805657ac403c6a7
Kun Yu Chen <96057963+ouo314@users.noreply.github.com>
2024-08-07 20:41:35 +0800

    adjust test signal

--- ESP32_scope.ino
+++ ESP32_scope.ino
@@ -1,22 +1,27 @@
+const int analogPin = 36;    
+const int pwmPin = 39;       
 
-const int analogPin = 34;    
-const int pwmPin = 25;       
-
-// 設置PWM參數
 const int pwmFrequency = 1000; 
 const int pwmResolution = 8;  
-const int pwmDutyCycle = 128;  
+const int pwmDelay = 10;      
 
 void setup() {
   Serial.begin(115200);
-  analogReadResolution(12);
+  analogReadResolution(12);   
   ledcSetup(0, pwmFrequency, pwmResolution);
   ledcAttachPin(pwmPin, 0);
-  ledcWrite(0, pwmDutyCycle);
 }
 
 void loop() {
-  int adcValue = analogRead(analogPin);
-  Serial.println(adcValue);  
-  delay(10); 
+  for(int i = 0; i < 256; i++) {
+    ledcWrite(0, i);          
+    int adcValue = analogRead(analogPin); 
+    Serial.print("PWM: ");
+    Serial.print(i);
+    Serial.print(" ADC: ");
+    Serial.println(adcValue);  
+    delay(pwmDelay);          
+  }
+  
+  delay(100);                
 }
